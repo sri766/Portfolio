43085be0813721f403a24660e9abecfd71bd8c06
sri766 <srisanthseth28@gmail.com>
2023-08-26 17:36:17 +0530

    added project style to mobile interface

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,19 +8,19 @@ const Contact = () => {
       <h1>Let's Connect</h1>
       <ul>
         <li>
-          <a href='https://twitter.com/SrisanthSeth' target="_blank" rel="noreferrer noopener">Twitter </a>
+          <a href='https://twitter.com/SrisanthSeth' target="_blank" rel="noreferrer noopener"><span>Twitter</span></a>
           <img src="./images/twitter.png" alt="twitter" style={{ height:'30px'}}></img>
         </li>
         <li>
-          <a href='https://www.instagram.com/sri_santh._/' target="_blank" rel="noreferrer noopener">Instagram</a>
+          <a href='https://www.instagram.com/sri_santh._/' target="_blank" rel="noreferrer noopener"><span>Instagram</span></a>
           <img src="./images/instagram.png" alt="instagram" style={{ height:'30px'}}></img>
         </li>
         <li>
-          <a href='https://www.linkedin.com/in/srisanth-seth-51b54b225/' target="_blank" rel="noreferrer noopener">LinkedIn</a>
+          <a href='https://www.linkedin.com/in/srisanth-seth-51b54b225/' target="_blank" rel="noreferrer noopener"><span>Linkedin</span></a>
           <img src="./images/linkedin.png" alt="linkedin" style={{ height:'30px'}}></img>
         </li>
         <li>
-          <a href='https://github.com/sri766' target="_blank" rel="noreferrer noopener">Github</a>
+          <a href='https://github.com/sri766' target="_blank" rel="noreferrer noopener"><span>GitHub</span></a>
           <img src="./images/github.png" alt="github" style={{ height:'30px'}}></img>
         </li>
       </ul>
